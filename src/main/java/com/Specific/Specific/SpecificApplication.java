eb7906b65bac5122ce29fc076d4645b1be9eb3c9
nurmukhammed <nurmuhammedkalmamatov06@gmail.com>
2025-04-21 17:39:26 +0600

    NEw

---
+++ src/main/java/com/Specific/Specific/SpecificApplication.java
@@ -0,0 +1,13 @@
+package com.Specific.Specific;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecificApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecificApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Specific/Specific/SpecificApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Specific.Specific;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecificApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
